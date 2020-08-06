4e420bbf615388b9b0590d8536572527cb206b1f
xuxinhong <xuxh@xuxinhongs-MacBook-Pro-2.local>
2020-08-06 17:51:05 +0800

    Initial commit

---
+++ test_rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+# @Create    : 2020/8/3   
+# @Author  : xuxh
+# @Description   :  说明文件功能
+# @Modify Time      @Version    @Description
+------------      --------    -----------
+2020/8/3             1.0         None
+"""
+
+
+from rectangle import Rectangle
+import pytest
+from time import sleep
+
+
+@pytest.fixture()
+def my_rect():
+    rect = Rectangle(30, 15)
+    return rect
+
+
+@pytest.mark.smoke
+@pytest.mark.p1
+def test_area(my_rect):
+    sleep(10)
+    assert my_rect.area() == 450
+
+
+@pytest.mark.regression
+@pytest.mark.repeat(5)
+def test_perimeter(my_rect):
+    sleep(10)
+    assert my_rect.perimeter() == 90
+
+
+@pytest.mark.parametrize(("length", "width", "expected_diff"), [(30, 20, 10), (20, 20, 0)])
+def test_diff(length, width, expected_diff):
+    sleep(10)
+    rect = Rectangle(length, width)
+    assert rect.diff() == expected_diff
+
+
